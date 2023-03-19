47882f4e21fc224c6db4458bccf1da63a7935ce5
Kristian Hennings <krhe@inventors.dk>
2023-03-19 15:16:37 +0100

    Started on the spAutism protocol

---
+++ docs/spAutism/Functions.py
@@ -0,0 +1,12 @@
+from System import Random
+
+def CreateRandom():
+    return Random()
+
+
+def GetIntensity():
+    if random.Next(2) == 1:
+        return Is + x
+    else:
+        return Is - x
+
