0d76c2145ed1b7e868f5ceb1e75cb5e9bbb5909d
Sophie Clarke <137508732+sophieclarke3@users.noreply.github.com>
2023-08-03 13:51:30 +0100

    NWR protocol (#10)
    * Change cuff pressure to 70%
    
    * Changed stim intensity dependencies

--- docs/condnwrs/Functions.py
+++ docs/condnwrs/Functions.py
@@ -6,7 +6,7 @@ def Condition(tc):
     chan = algometer.Channels[0]
 
     chan.SetStimulus(1, chan.CreateWaveform()
-                     .Step(tc.SR.PTT, 9.9 * 60))
+                     .Step(tc.SR.PTT*0.70, 9.9 * 60))
     algometer.ConfigurePressureOutput(0, ChannelID.CH01)
     algometer.StartStimulation(AlgometerStopCriterion.STOP_CRITERION_ON_BUTTON_PRESSED, True)
 
