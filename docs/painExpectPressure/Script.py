457dfd0b778d457b3a882cc4570b158171d5ca74
Kristian Hennings <krhe@inventors.dk>
2023-09-03 16:48:13 +0200

    Work on the script for the experimental paradigm
    Now display of images work

--- docs/painExpectPressure/Script.py
+++ docs/painExpectPressure/Script.py
@@ -1,5 +1,6 @@
 ﻿from Serilog import Log
 from LabBench.Interface.Instruments.Algometry import *
+import traceback
 
 def Stimulate(tc, x):
     algometer = tc.Devices.Algometer
@@ -16,58 +17,60 @@ class ImageRepository:
     def __init__(self, tc):
         stimuli = tc.Assets.VisualStimuli
         self._images = {
-                "Marker",                   stimuli.GetAsset("Marker.png"),
-                "MarkerWithFiducial",       stimuli.GetAsset("MarkerWithFiducial.png"),
-                "High73",                   stimuli.GetAsset("High73.png"),
-                "High79",                   stimuli.GetAsset("High79.png"),
-                "High84",                   stimuli.GetAsset("High84.png"),
-                "High88",                   stimuli.GetAsset("High88.png"),
-                "High93",                   stimuli.GetAsset("High93.png"),
-                "HighCue1",                 stimuli.GetAsset("HighCue1.png"),
-                "HighCue2",                 stimuli.GetAsset("HighCue2.png"),
-                "LearningRateExpectedPain", stimuli.GetAsset("LearningRateExpectedPainDE.png") 
+                "Blank":                    stimuli.GetAsset("Blank.png"),
+                "Marker":                   stimuli.GetAsset("Marker.png"),
+                "MarkerWithFiducial":       stimuli.GetAsset("MarkerWithFiducial.png"),
+                "High73":                   stimuli.GetAsset("High73.png"),
+                "High79":                   stimuli.GetAsset("High79.png"),
+                "High84":                   stimuli.GetAsset("High84.png"),
+                "High88":                   stimuli.GetAsset("High88.png"),
+                "High93":                   stimuli.GetAsset("High93.png"),
+                "HighCue1":                 stimuli.GetAsset("HighCue1.png"),
+                "HighCue2":                 stimuli.GetAsset("HighCue2.png"),
+                "LearningRateExpectedPain": stimuli.GetAsset("LearningRateExpectedPainDE.png") 
                                             if tc.Language == "de" else 
                                             stimuli.GetAsset("LearningRateExpectedPainEn.png"),
-                "Low27",                    stimuli.GetAsset("Low27.png"),
-                "Low32",                    stimuli.GetAsset("Low32.png"),
-                "Low40",                    stimuli.GetAsset("Low40.png"),
-                "Low45",                    stimuli.GetAsset("Low45.png"),
-                "Low51",                    stimuli.GetAsset("Low51.png"),
-                "LowCue1",                  stimuli.GetAsset("LowCue1.png"),
-                "LowCue2",                  stimuli.GetAsset("LowCue2.png"),
-                "TestRateExpectedPain",     stimuli.GetAsset("TestRateExpectedPainDE.png") 
+                "Low27":                    stimuli.GetAsset("Low27.png"),
+                "Low32":                    stimuli.GetAsset("Low32.png"),
+                "Low40":                    stimuli.GetAsset("Low40.png"),
+                "Low45":                    stimuli.GetAsset("Low45.png"),
+                "Low51":                    stimuli.GetAsset("Low51.png"),
+                "LowCue1":                  stimuli.GetAsset("LowCue1.png"),
+                "LowCue2":                  stimuli.GetAsset("LowCue2.png"),
+                "TestRateExpectedPain":     stimuli.GetAsset("TestRateExpectedPainDE.png") 
                                             if tc.Language == "de" else 
                                             stimuli.GetAsset("TestRateExpectedPainEN.png"),
-                "TestRatePain",             stimuli.GetAsset("TestRatePainDE.png") 
+                "TestRatePain":             stimuli.GetAsset("TestRatePainDE.png") 
                                             if tc.Language == "de" else 
                                             stimuli.GetAsset("TestRatePainEN.png")
             }
         
     def get(self, id):
-        return self.images[id]
+        return self._images[id].Data
         
 class LearningTask:
     def __init__(self, tc):
-        self.images = ImageRepository(tc)
+        self._images = ImageRepository(tc)
         
     def reset(self):
+        Log.Information("Learning task has been reset")
         return True
         
     def nextTrial():
         pass
     
     def getImage(self, id):
-        return self.images.get(id)
+        return self._images.get(id)
     
     def getCue(self):
-        return self.images.get("HighCue1")
+        return self._images.get("HighCue1")
     
     def getFeedback(self):
-        return self.images.get("High88")
+        return self._images.get("High88")
 
 class TestTask:
     def __init__(self, tc):
-        self.images = ImageRepository(tc)
+        self._images = ImageRepository(tc)
         
     def reset(self):
         return True
@@ -76,13 +79,13 @@ class TestTask:
         pass
     
     def getImage(self, id):
-        return self.images.get(id)
+        return self._images.get(id)
     
     def getCue(self):
-        return self.images.get("HighCue1")
+        return self._images.get("HighCue1")
     
     def stimulate(self, tc):
-        return self.images.get("MarkerWithFiducial")    
+        return self._images.get("MarkerWithFiducial")    
 
 def CreateLearningTask(tc):
     return LearningTask(tc)
@@ -91,22 +94,29 @@ def InitializeLearning(tc):
     return tc.LearningTask.reset()
 
 def RunLearning(tc, x):
-    display = tc.Devices.Display
-    task = tc.LearningTask
+    retValue = True
+    
+    try:
+        display = tc.Devices.Display
+        task = tc.LearningTask
+        Log.Information("Running the learning task for step: {name}".format(name = tc.StimulusName))
 
-    if (tc.StimulusName == "Marker"):
-        display.Display(task.getImage("Marker"))
-    elif (tc.StimulusName == "Cue"):
-        display.Display(task.getCue())
-    elif (tc.StimulusName == "RateExpected"):
-        display.Display(task.getImage("LearningRateExpectedPain"))
-    elif (tc.StimulusName == "Feedback"):
-        display.Display(task.getFeedback())
-    elif (tc.StimulusName == "Blank"):
-        display.Display(task.getImage("Blank"))
-        task.nextTrial()
+        if (tc.StimulusName == "Marker"):
+            display.Display(task.getImage("Marker"))
+        elif (tc.StimulusName == "Cue"):
+            display.Display(task.getCue())
+        elif (tc.StimulusName == "RateExpected"):
+            display.Display(task.getImage("LearningRateExpectedPain"))
+        elif (tc.StimulusName == "Feedback"):
+            display.Display(task.getFeedback())
+        elif (tc.StimulusName == "Blank"):
+            display.Display(task.getImage("Blank"))
+            task.nextTrial()
+    except Exception as e:
+        Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
+        retValue = False
         
-    return True
+    return retValue
 
 def CreateTestTask(tc):
     return TestTask(tc)
@@ -115,25 +125,31 @@ def InitializeTest(tc):
     return tc.TestTask.reset()
 
 def RunTest(tc, x):
-    display = tc.Devices.Display
-    task = tc.TestTask
+    retValue = True
+    
+    try:
+        display = tc.Devices.Display
+        task = tc.TestTask
 
-    if (tc.StimulusName == "Marker1"):
-        display.Display(task.getImage("Marker"))
-    elif (tc.StimulusName == "Cue"):
-        display.Display(task.getCue())
-    elif (tc.StimulusName == "RateExpected"):
-        display.Display(task.getImage("TestRateExpectedPain"))
-    elif (tc.StimulusName == "Marker2"):
-        display.Display(task.getImage("Marker"))
-    elif (tc.StimulusName == "Stimulate"):
-        display.Display(task.stimulate(tc))
-    elif (tc.StimulusName == "Marker3"):
-        display.Display(task.getImage("Marker"))
-    elif (tc.StimulusName == "Rate"):
-        display.Display(task.getImage("TestRatePain"))
-    elif (tc.StimulusName == "Blank"):
-        display.Display(task.getImage("Blank"))
-        task.nextTrial()
+        if (tc.StimulusName == "Marker1"):
+            display.Display(task.getImage("Marker"))
+        elif (tc.StimulusName == "Cue"):
+            display.Display(task.getCue())
+        elif (tc.StimulusName == "RateExpected"):
+            display.Display(task.getImage("TestRateExpectedPain"))
+        elif (tc.StimulusName == "Marker2"):
+            display.Display(task.getImage("Marker"))
+        elif (tc.StimulusName == "Stimulate"):
+            display.Display(task.stimulate(tc))
+        elif (tc.StimulusName == "Marker3"):
+            display.Display(task.getImage("Marker"))
+        elif (tc.StimulusName == "Rate"):
+            display.Display(task.getImage("TestRatePain"))
+        elif (tc.StimulusName == "Blank"):
+            display.Display(task.getImage("Blank"))
+            task.nextTrial()
+    except Exception as e:
+        Log.Error("An exception {e}: {trace}".format(e = e, trace = traceback.format_exc()))
+        retValue = False
         
-    return True
+    return retValue
