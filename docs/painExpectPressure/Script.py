6ba7f17dca3e28352483204a78288d3462656e15
Kristian Hennings <krhe@inventors.dk>
2023-09-03 10:56:03 +0200

    Created stimulus presentation test

--- docs/painExpectPressure/Script.py
+++ docs/painExpectPressure/Script.py
@@ -1,1 +1,13 @@
-﻿
+﻿from Serilog import Log
+from LabBench.Interface.Instruments.Algometry import *
+
+def Stimulate(tc, x):
+    algometer = tc.Devices.Algometer
+    chan = algometer.Channels[0]
+
+    chan.SetStimulus(1, chan.CreateWaveform()
+                     .Step(x, 1))
+    algometer.ConfigurePressureOutput(0, ChannelID.CH01)
+    algometer.StartStimulation(AlgometerStopCriterion.STOP_CRITERION_ON_BUTTON_PRESSED, True)
+
+    return True
