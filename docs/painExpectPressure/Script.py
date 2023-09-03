078907b0e0e4a68568902119fb367c1d104ba1fd
Kristian Hennings <krhe@inventors.dk>
2023-09-03 18:09:25 +0200

    Simplified stim sequence
    However, this result in the stimuli going out of synch.
    
    Must be fixed in LabBench.

--- docs/painExpectPressure/Script.py
+++ docs/painExpectPressure/Script.py
@@ -1,6 +1,7 @@
 ﻿from Serilog import Log
 from LabBench.Interface.Instruments.Algometry import *
 import traceback
+import random
 
 def Stimulate(tc, x):
     algometer = tc.Devices.Algometer
@@ -47,26 +48,117 @@ class ImageRepository:
         
     def get(self, id):
         return self._images[id].Data
+    
+    def getHighCue(self, variant):
+        if variant == 0:
+            return self._images['HighCue1'].Data
+        elif variant == 1:
+            return self._images['HighCue2'].Data
+        else:
+            raise ValueError("Invalid cue variant: {variant}".format(variant = variant)) 
         
+    def getLowCue(self, variant):
+        if variant == 0:
+            return self._images['LowCue1'].Data
+        elif variant == 1:
+            return self._images['LowCue2'].Data
+        else:
+            raise ValueError("Invalid cue variant: {variant}".format(variant = variant)) 
+        
+    def getHighFeedback(self, variant):
+        if variant == 0:
+            return self._images['High73'].Data
+        elif variant == 1:
+            return self._images['High79'].Data
+        elif variant == 2:
+            return self._images['High84'].Data
+        elif variant == 3:
+            return self._images['High88'].Data
+        elif variant == 4:
+            return self._images['High93'].Data
+        else:
+            raise ValueError("Invalid feedback variant: {variant}".format(variant = variant)) 
+        
+    
+    def getLowFeedback(self, variant):
+        if variant == 0:
+            return self._images['Low27'].Data
+        elif variant == 1:
+            return self._images['Low32'].Data
+        elif variant == 2:
+            return self._images['Low40'].Data
+        elif variant == 3:
+            return self._images['Low45'].Data
+        elif variant == 4:
+            return self._images['Low51'].Data
+        else:
+            raise ValueError("Invalid feedback variant: {variant}".format(variant = variant))         
+        
+        
+class LearningTrial:
+    def __init__(self, high, variant):
+        self.high = high
+        self.variant = variant
+        self.cue = random.randint(0, 4)
+        
+    def getCue(self, images):
+        return images.getHighCue(self.variant) if self.high == 1 else images.getLowCue(self.variant)
+                
+    def getFeedback(self, images):
+        if self.high == 1:
+            return images.getHighFeedback(self.cue)
+        else:
+            return images.getLowFeedback(self.cue)      
+
 class LearningTask:
     def __init__(self, tc):
         self._images = ImageRepository(tc)
+        self._iteration = 0
+        self._trials = []
+        numberOfBlocks = tc.NumberOfLearningTrials / 4
+        
+        for n in range(numberOfBlocks):
+            block = [LearningTrial(1, 0), 
+                     LearningTrial(1, 1), 
+                     LearningTrial(0, 0), 
+                     LearningTrial(0, 1)]
+            random.shuffle(block)
+            
+            for trial in block:
+                self._trials.append(trial)
         
     def reset(self):
-        Log.Information("Learning task has been reset")
+        Log.Information("Resetting learning task with {n} trials".format(n = len(self._trials)))
+        
+        self._iteration = 0
         return True
         
-    def nextTrial():
-        pass
+    def printTrial(self):
+        if (self._iteration < len(self._trials)):
+            current = self._trials[self._iteration]
+            Log.Information("TRIAL (high = {high}, variant = {variant}, cue = {cue})".format(high = current.high, 
+                                                                                             variant = current.variant, 
+                                                                                             cue = current.cue))
+        
+    def nextTrial(self):
+        self._iteration = self._iteration + 1
     
     def getImage(self, id):
         return self._images.get(id)
     
     def getCue(self):
-        return self._images.get("HighCue1")
+        if (self._iteration >= len(self._trials)):
+            raise ValueError("No trial for iteration: {n}".format(n = self._iteration))
+
+        current = self._trials[self._iteration]
+        return current.getCue(self._images)
     
     def getFeedback(self):
-        return self._images.get("High88")
+        if (self._iteration >= len(self._trials)):
+            raise ValueError("No trial for iteration: {n}".format(n = self._iteration))
+
+        current = self._trials[self._iteration]
+        return current.getFeedback(self._images)
 
 class TestTask:
     def __init__(self, tc):
