6198fd0be3700f20b9ac6310bbd6f3b10971d7a8
Kristian Hennings <krhe@inventors.dk>
2023-10-24 12:41:19 +0200

    Added PCS

---
+++ docs/icePain/TEXT_DA.py
@@ -0,0 +1,31 @@
+def CreateText(tc):
+   	return {
+		"QUESTION": "Når jeg har smerte ...",
+		"I01": "Det bekymrer mig hele tiden, om smerterne vil forsvinde",
+		"I02": "Jeg føler, at jeg ikke kan mere",
+		"I03": "Det er frygteligt, og jeg tænker, at det aldrig bliver bedre",
+		"I04": "Det er forfærdeligt, og jeg føler mig overvældet af smerterne",
+		"I05": "Jeg føler, at jeg ikke kan holde det ud længere",
+		"I06": "Jeg bliver bange for at smerterne bliver værre",
+		"I07": "Jeg tænker hele tiden på andre smertefulde oplevelser",
+		"I08": "Jeg ønsker desperat, at smerterne vil forsvinde",
+		"I09": "Jeg kan ikke lade være med at tænke på mine smerter",
+		"I10": "Jeg bliver ved med at tænke på hvor ondt det gør",
+		"I11": "Jeg bliver ved med at tænke på, hvor meget jeg ønsker, at smerterne skal holde op",
+		"I12": "Der er intet, jeg kan gøre for at mindske intensiteten af mine smerterne",
+		"I13": "Jeg tænker på, om der kunne ske noget alvorligt",
+		"L0": "Slet ikke",
+		"L1": "I ringe grad",
+		"L2": "I nogen grad",
+		"L3": "I høj grad",
+		"L4": "I meget høj grad"
+	}
+
+
+
+
+
+
+
+
+
