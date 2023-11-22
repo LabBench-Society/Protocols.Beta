c1c9a203ff682d8221318662f6e5d59d2159c02c
Enrico De Martino <106465324+Enrico80@users.noreply.github.com>
2023-11-22 09:25:10 +0100

    Added IT

---
+++ docs/icePain/TEXT_IT.py
@@ -0,0 +1,26 @@
+def CreateText(tc):
+   	return {
+		"PCS.QUESTION": "When I'm in pain ... (IT)",
+		"PCS.I01": "I worry all the time about whether the pain will end.(IT)",
+		"PCS.I02": "I feel I can't go on.(IT)",
+		"PCS.I03": "It's terrible and I think it's never going to get any better(IT)",
+		"PCS.I04": "It's awful and I feel that it overwhelms me(IT)",
+		"PCS.I05": "I feel I can't stand it anymore(IT)",
+		"PCS.I06": "I become afraid that the pain will get worse(IT)",
+		"PCS.I07": "I keep thinking of other painful events(IT)",
+		"PCS.I08": "I anxiously want the pain to go away(IT)",
+		"PCS.I09": "I can't seem to keep it out of my mind(IT)",
+		"PCS.I10": "I keep thinking about how much it hurts(IT)",
+		"PCS.I11": "I keep thinking about how badly I want the pain to stop(IT)",
+		"PCS.I12": "There's nothing I can do to reduce the intensity of the pain(IT)",
+		"PCS.I13": "I wonder whether something serious may happen",
+		"PCS.L0": "Not at all(IT)",
+		"PCS.L1": "To a slight degree(IT)",
+		"PCS.L2": "To a moderate degree(IT)",
+		"PCS.L3": "To a great degree(IT)",
+		"PCS.L4": "All the time(IT)"
+	}
+
+
+
+
