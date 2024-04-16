3ab507b1ce28e9956c46fcb196e2fdc49adec028
Kristian Hennings <krhe@inventors.dk>
2024-04-16 15:25:32 +0200

    Added pain effort protocol

---
+++ docs/painEffort/rename.bat
@@ -0,0 +1,25 @@
+cd Images
+
+move Slide1.PNG Blank.png
+move Slide2.PNG QuestionnaireInstruction.png
+move Slide3.PNG MVCInstruction.png
+move Slide4.PNG PainEffortInstruction01.png
+move Slide5.PNG PainEffortInstruction02.png
+move Slide6.PNG PainEffortInstruction03.png
+move Slide7.PNG Ready.png
+move Slide8.PNG Cue3.png
+move Slide9.PNG Cue2.png
+move Slide10.PNG Cue1.png
+move Slide11.PNG Contract.png
+move Slide12.PNG MVCPostEffort.png
+move Slide13.PNG Rest.png
+move Slide14.PNG Complete.png
+move Slide15.PNG RightLeg.png
+move Slide16.PNG LeftLeg.png
+move Slide17.PNG SR1.png
+move Slide18.PNG SR2.png
+move Slide19.PNG TS.png
+move Slide20.PNG CPM.png
+
+cd ..
+pause
