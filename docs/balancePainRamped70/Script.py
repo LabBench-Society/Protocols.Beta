d35ba7dbbdbae54a462d4fbbb3a7cdd71c63eab5
Kristian Hennings <krhe@inventors.dk>
2025-01-21 12:36:39 +0100

    Bugfixes to the stimulation script

--- docs/balancePainRamped70/Script.py
+++ docs/balancePainRamped70/Script.py
@@ -3,10 +3,10 @@ def StimulateArm(tc):
     algometer = tc.Instruments.PressureAlgometer
 
     chan = algometer.Channels[0]
-    chan.SetStimulus(1, chan.CreateWaveform().Ramp(tc.Intensity, 2).Step(tc.Intensity, 13))
+    chan.SetStimulus(1, chan.CreateWaveform().Increment(tc.Intensity / 2, 2).Step(tc.Intensity, 13))
 
     algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.CH01)
-    algometer.ConfigurePressureOutput(1, algometer.ChannelIDs.NONE)
+    algometer.ConfigurePressureOutput(1, algometer.ChannelIDs.NoChannel)
     algometer.StartStimulation(algometer.StopCriterions.WhenButtonPressed, True)
 
     return True
@@ -15,10 +15,16 @@ def StimulateLeg(tc):
     algometer = tc.Instruments.PressureAlgometer
 
     chan = algometer.Channels[0]
-    chan.SetStimulus(1, chan.CreateWaveform().Ramp(tc.Intensity, 2).Step(tc.Intensity, 13))
+    chan.SetStimulus(1, chan.CreateWaveform().Increment(tc.Intensity / 2, 2).Step(tc.Intensity, 13))
 
-    algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.NONE)
+    algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.NoChannel)
     algometer.ConfigurePressureOutput(1, algometer.ChannelIDs.CH01)
     algometer.StartStimulation(algometer.StopCriterions.WhenButtonPressed, True)
+
+    return True
+
+def StopStimulation(tc):
+    algometer = tc.Instruments.PressureAlgometer
+    algometer.StopStimulation()
 
     return True
