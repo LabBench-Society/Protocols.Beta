618afcb6179e88d4db21e0b0bec6a60a8e6e83db
Kristian Hennings <krhe@inventors.dk>
2025-03-26 14:11:30 +0100

    Fixed conditioning stimulus

--- docs/heidelberg.schmerz.superNova.v1/Script.py
+++ docs/heidelberg.schmerz.superNova.v1/Script.py
@@ -3,12 +3,15 @@
 def Stimulate(tc):
     algometer = tc.Instruments.PressureAlgometer
     display = tc.Instruments.ImageDisplay
-
+    intensity = 0.7 * tc.NDLEG.PTT
+    time = 110 - (6 + tc.CPM.PTT)
+    
     chan = algometer.Channels[0]
-    chan.SetStimulus(1, chan.CreateWaveform().Step(0.7 * tc.NDLEG.PTT, 110 - (6 + tc.CPM.PTT)))
+    chan.SetStimulus(1, chan.CreateWaveform().Step(intensity, time))
 
-    algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.NONE)
+    algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.NoChannel)
     algometer.ConfigurePressureOutput(1, algometer.ChannelIDs.CH02)
     algometer.StartStimulation(algometer.StopCriterions.WhenButtonPressed, True, False)
+    tc.Log.Information("STIMULATION STARTED [ Intensity: {intensity}, Time: {time}]", intensity, time)
 
     return True
