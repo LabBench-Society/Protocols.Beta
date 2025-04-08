88bb947539c27b415586da23da1ef14699377d7e
Kristian Hennings <krhe@inventors.dk>
2025-04-08 19:49:59 +0200

    Created version 2 of the SuperNova protocol

---
+++ docs/heidelberg.schmerz.superNova.v2/CPAInstructions.bat
@@ -0,0 +1,11 @@
+cd CPAInstructions
+move Slide1.PNG WUSSP.png
+move Slide2.PNG DLEG.png
+move Slide3.PNG NDLEG.png
+move Slide4.PNG SSP.png
+move Slide5.PNG TS.png
+move Slide6.PNG CPM.png
+
+cd ..
+
+pause
---
+++ docs/heidelberg.schmerz.superNova.v2/Script.py
@@ -0,0 +1,17 @@
+
+
+def Stimulate(tc):
+    algometer = tc.Instruments.PressureAlgometer
+    display = tc.Instruments.ImageDisplay
+    intensity = 0.7 * tc.NDLEG.PTT
+    time = 110 - (6 + tc.CPM.PTT)
+    
+    chan = algometer.Channels[0]
+    chan.SetStimulus(1, chan.CreateWaveform().Step(intensity, time))
+
+    algometer.ConfigurePressureOutput(0, algometer.ChannelIDs.NoChannel)
+    algometer.ConfigurePressureOutput(1, algometer.ChannelIDs.CH02)
+    algometer.StartStimulation(algometer.StopCriterions.WhenButtonPressed, True, False)
+    tc.Log.Information("STIMULATION STARTED [ Intensity: {intensity}, Time: {time}]", intensity, time)
+
+    return True
