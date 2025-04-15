c75d630c48776d868af96ee411ced605acff888c
Kristian Hennings <krhe@inventors.dk>
2025-04-15 17:09:28 +0200

    Added subject control of pain areas and pain rating

---
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_EN.py
@@ -0,0 +1,11 @@
+def Create(tc):
+   return {
+      "Q01Title": "Pain location",
+      "Q01Instruction": "Please select all the areas in which you feel pain",
+      "Q02Title": "Worst pain location",
+      "Q02Instruction": "Please select the area where you feel the most pain",
+      "Q03Title": "Pain intensity",
+      "Q03Instruction": "Please rate the intensity of your pain",
+      "Q03BottomAnchor": "No Pain",
+      "Q03TopAnchor": "Maximal Pain"
+   }
---
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_NL.py
@@ -0,0 +1,11 @@
+def Create(tc):
+   return {
+      "Q01Title": "Pain location (Translate to Dutch)",
+      "Q01Instruction": "Please select all the areas in which you feel pain (Translate to Dutch)",
+      "Q02Title": "Worst pain location (Translate to Dutch)",
+      "Q02Instruction": "Please select the area where you feel the most pain (Translate to Dutch)",
+      "Q03Title": "Pain intensity (Translate to Dutch)",
+      "Q03Instruction": "Please rate the intensity of your pain (Translate to Dutch)",
+      "Q03BottomAnchor": "No Pain (Translate to Dutch)",
+      "Q03TopAnchor": "Maximal Pain (Translate to Dutch)"
+   }
