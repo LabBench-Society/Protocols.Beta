87693954086bef37f9a3368ccc49390b0a29e527
Kristian Hennings <krhe@inventors.dk>
2025-04-23 10:57:27 +0200

    Updated questionnaire and added dutch translations

--- docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_EN.py
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_EN.py
@@ -1,11 +1,24 @@
 def Create(tc):
    return {
-      "Q01Title": "Pain location",
-      "Q01Instruction": "Please select all the areas in which you feel pain",
-      "Q02Title": "Worst pain location",
-      "Q02Instruction": "Please select the area where you feel the most pain",
-      "Q03Title": "Pain intensity",
-      "Q03Instruction": "Please rate the intensity of your pain",
-      "Q03BottomAnchor": "No Pain",
-      "Q03TopAnchor": "Maximal Pain"
+      "Q01Title": "Overall pain",
+      "Q01Instruction": "Do you have pain right now?",
+      "Q02Title": "Pain distribution",
+      "Q02Instruction": "Where do you have pain right now?",
+      "Q03Title": "Overall pain intensity",
+      "Q03Instruction": "For the regions you just indicated, what is the average intensity of your pain right now?",
+      "Q04Title": "Peak pain location",
+      "Q04Instruction": "Where do you have the most pain right now?",
+      "Q05Title": "Overall pain intensity ",
+      "Q05Instruction": "For the region(s) you just indicated as the most painful, what is the intensity of your pain right now?",
+      "Q06Title": "Low back pain",
+      "Q06Instruction": "Do you have low back pain right now?",
+      "Q07Title": "Low back pain intensity",
+      "Q07Instruction": "What is the intensity of your low back pain right now?",
+      "Q08Title": "Low back pain unpleasantness",
+      "Q08Instruction": "How unpleasant is your low back pain right now?",
+
+      "PainBottomAnchor": "No Pain",
+      "PainTopAnchor": "Worst pain you can imagine",
+      "UnpleasentnessBottomAnchor": "Not at all unpleasant",
+      "UnpleasentnessTopAnchor": "Very unpleasant"
    }
--- docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_NL.py
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP2.v1/Text_NL.py
@@ -1,11 +1,24 @@
 def Create(tc):
    return {
-      "Q01Title": "Pain location (Translate to Dutch)",
-      "Q01Instruction": "Please select all the areas in which you feel pain (Translate to Dutch)",
-      "Q02Title": "Worst pain location (Translate to Dutch)",
-      "Q02Instruction": "Please select the area where you feel the most pain (Translate to Dutch)",
-      "Q03Title": "Pain intensity (Translate to Dutch)",
-      "Q03Instruction": "Please rate the intensity of your pain (Translate to Dutch)",
-      "Q03BottomAnchor": "No Pain (Translate to Dutch)",
-      "Q03TopAnchor": "Maximal Pain (Translate to Dutch)"
+      "Q01Title": "Overall pain",
+      "Q01Instruction": "Heb je pijn op dit moment?",
+      "Q02Title": "Pain distribution",
+      "Q02Instruction": "Waar heb je pijn op dit moment?",
+      "Q03Title": "Overall pain intensity",
+      "Q03Instruction": "Over de regio’s die je zojuist hebt aangeduid, wat is de gemiddelde intensiteit van je pijn op dit moment?",
+      "Q04Title": "Peak pain location",
+      "Q04Instruction": "Waar heb je het meeste pijn op dit moment?",
+      "Q05Title": "Overall pain intensity ",
+      "Q05Instruction": "Voor de regio(’s) die je zojuist als het meest pijnlijk hebt aangeduid, wat is de intensiteit van je pijn op dit moment?",
+      "Q06Title": "Low back pain",
+      "Q06Instruction": "Heb je lage rugpijn op dit moment?",
+      "Q07Title": "Low back pain intensity",
+      "Q07Instruction": "Wat is de intensiteit van je lage rugpijn op dit moment?",
+      "Q08Title": "Low back pain unpleasantness",
+      "Q08Instruction": "Hoe onaangenaam is je lage rugpijn op dit moment?",
+
+      "PainBottomAnchor": "Geen pijn",
+      "PainTopAnchor": "Ergste pijn die u zich kunt voorstellen",
+      "UnpleasentnessBottomAnchor": "Helemaal niet onaangenaam",
+      "UnpleasentnessTopAnchor": "Heel onaangenaam"
    }
