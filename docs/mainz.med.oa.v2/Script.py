eb1b79be2777efaecf0cd6c045fe49206c6ff372
Kristian Hennings <krhe@inventors.dk>
2025-10-09 15:44:24 +0200

    OA protocol

--- docs/mainz.med.oa.v2/Script.py
+++ docs/mainz.med.oa.v2/Script.py
@@ -1,14 +1,17 @@
 
 class OffsetAnalgesia:
-   def __init__(self, tc, intensity, duration):
+   def __init__(self, tc, conditioning, intensity, duration):
       self.tc = tc
-      self.PreDuration = duration[0]
-      self.StimulusDuration = duration[1]
-      self.PostDuration = duration[2]
+      self.PreConditioning = duration[0]
+      self.PreDuration = duration[1]
+      self.StimulusDuration = duration[2]
+      self.PostDuration = duration[3]
       self.Intensity = intensity
+      self.PrecondIntensity = 0.8
+      self.CondPressure = conditioning
 
    def Duration(self):
-      return self.PreDuration + self.StimulusDuration + self.PostDuration
+      return self.PreConditioning + self.PreDuration + self.StimulusDuration + self.PostDuration + 4
 
    def Stimulate(self, sr):
       cpar = self.tc.Instruments.PressureAlgometer
@@ -17,11 +20,13 @@ class OffsetAnalgesia:
       PDT = sr.PDT
       PTT = sr.PTT
 
-      CondIntensity = self.tc.CondPressure * (PTT - PDT) + PDT
+      CondIntensity = self.CondPressure * (PTT - PDT) + PDT
+      Intensity = self.Intensity * (PTT - PDT) + PDT
 
       channel.SetStimulus(1, channel.CreateWaveform()
+                        .Step(self.PrecondIntensity * PDT,self.PreConditioning)
                         .Step(CondIntensity,self.PreDuration)
-                        .Step(self.Intensity * PTT,self.StimulusDuration)
+                        .Step(Intensity,self.StimulusDuration)
                         .Step(CondIntensity,self.PostDuration))
       cpar.ConfigurePressureOutput(0, cpar.ChannelIDs.CH01)
       cpar.ConfigurePressureOutput(1, cpar.ChannelIDs.NoChannel)
@@ -36,9 +41,10 @@ class OffsetAnalgesia:
       PDT = sr.PDT
       PTT = sr.PTT
 
-      CondIntensity = self.tc.CondPressure * (PTT - PDT) + PDT
+      CondIntensity = self.CondPressure * (PTT - PDT) + PDT
 
       channel.SetStimulus(1, channel.CreateWaveform()
+                        .Step(self.PrecondIntensity * PDT,self.PreConditioning)
                         .Step(CondIntensity,self.PreDuration)
                         .Step(CondIntensity,self.StimulusDuration)
                         .Step(CondIntensity,self.PostDuration))
@@ -48,8 +54,17 @@ class OffsetAnalgesia:
 
       return True
    
-def CreateThresholdVersion(tc):
-   return OffsetAnalgesia(tc, 1, [5, 2, 5])
+def CreateOffsetAnalgesia(tc):
+   return OffsetAnalgesia(tc, 0.4, 1, [2, 5, 2, 5])
+
+def CreateOffsetHyperAnalgesia(tc):
+   return OffsetAnalgesia(tc, 0.8, 0.2, [2, 5, 2, 5])
+
+def CreateLongOffsetAnalgesia(tc):
+   return OffsetAnalgesia(tc, 0.4, 1, [2, 10, 10, 10])
+
+def CreateLongOffsetHyperAnalgesia(tc):
+   return OffsetAnalgesia(tc, 0.8, 0.2, [2, 10, 10, 10])
 
 def Stop(tc):
    cpar = tc.Instruments.PressureAlgometer
