10e7064aa32634a658ed101e29d4c82d899a1301
Kristian Hennings <krhe@inventors.dk>
2025-11-11 14:09:31 +0100

    Deleted the old WP1 and replaced it with WP2 protocol

--- docs/kuleuven.gbiomed.LaBGAS.SynapsWP1.v1/CPAInstructions.bat
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP1.v1/CPAInstructions.bat
@@ -1,14 +1,16 @@
 cd CPAInstructions_EN
 move Slide1.PNG SR01.png
 move Slide2.PNG TS.png
-move Slide3.PNG CPM.png
+move Slide3.PNG HOT.png
+move Slide4.PNG CPM.png
 
 cd ..
 
 cd CPAInstructions_NL
 move Slide1.PNG SR01.png
 move Slide2.PNG TS.png
-move Slide3.PNG CPM.png
+move Slide3.PNG HOT.png
+move Slide4.PNG CPM.png
 
 cd ..
 
---
+++ docs/kuleuven.gbiomed.LaBGAS.SynapsWP1.v1/Text_NL.py
@@ -0,0 +1,26 @@
+def Create(tc):
+   return {
+      "Q01Title": "Algemene pijn",
+      "Q01Instruction": "Heb je pijn op dit moment?",
+      "Q02Title": "Pijnlocatie",
+      "Q02Instruction": "Waar heb je pijn op dit moment?",
+      "Q03Title": "Algemene pijnintensiteit",
+      "Q03Instruction": "Over de regio’s die je zojuist hebt aangeduid, wat is de gemiddelde intensiteit van je pijn op dit moment?",
+      "Q04Title": "Maximale pijnlocatie",
+      "Q04Instruction": "Waar heb je het meeste pijn op dit moment?",
+      "Q05Title": "Maximale pijnintensiteit",
+      "Q05Instruction": "Voor de regio(’s) die je zojuist als het meest pijnlijk hebt aangeduid, wat is de intensiteit van je pijn op dit moment?",
+      "Q06Title": "Lage rugpijn",
+      "Q06Instruction": "Heb je lage rugpijn op dit moment?",
+      "Q07Title": "Lage rugpijnintensiteit",
+      "Q07Instruction": "Wat is de intensiteit van je lage rugpijn op dit moment?",
+      "Q08Title": "Lage rugpijnonaangenaamheid",
+      "Q08Instruction": "Hoe onaangenaam is je lage rugpijn op dit moment?",
+      "Q09Title": "Dominante been",
+      "Q09Instruction": "Wat is je dominante been (het been waarmee je een bal zou trappen)?",
+
+      "PainBottomAnchor": "Geen pijn",
+      "PainTopAnchor": "Ergste pijn die u zich kunt voorstellen",
+      "UnpleasentnessBottomAnchor": "Helemaal niet onaangenaam",
+      "UnpleasentnessTopAnchor": "Heel onaangenaam"
+   }
