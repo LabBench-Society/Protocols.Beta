a25fe232385791d266c4d8b39e1c5157ce0edc91
Kristian Hennings <krhe@inventors.dk>
2025-12-01 12:06:35 +0100

    Fixed log message + coding style (more concise)

--- docs/essex.pptReliabilityCPMcCPMpPTT/Functions.py
+++ docs/essex.pptReliabilityCPMcCPMpPTT/Functions.py
@@ -5,13 +5,12 @@ def Condition(tc):
     algometer = tc.Instruments.PressureAlgometer
     chan = algometer.Channels[0]
 
-    chan.SetStimulus(1, chan.CreateWaveform()
-                     .Step(tc.SR.PTT*0.70, 9.9 * 60))
-    algometer.ConfigurePressureOutput(0, ChannelID.CH01)
-    algometer.ConfigurePressureOutput(1, ChannelID.NONE)
+    chan.SetStimulus(1, chan.CreateWaveform().Step(tc.SR02.PTT*0.70, 9.9 * 60))
+    algometer.ConfigurePressureOutput(0, ChannelID.NONE)
+    algometer.ConfigurePressureOutput(1, ChannelID.CH01)
     algometer.StartStimulation(AlgometerStopCriterion.STOP_CRITERION_ON_BUTTON_PRESSED, True)
 
-    Log.Information("Starting conditioning: {intensity}", tc.SR.PTT)
+    Log.Information("Starting conditioning: {intensity}", tc.SR02.PTT * 0.70)
 
     return True
 
