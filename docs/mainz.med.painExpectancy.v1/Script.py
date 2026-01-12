e6d7d945d9075ccf84a8d7af05441b62eaa2d0a8
Kristian Hennings <krhe@inventors.dk>
2026-01-12 16:43:38 +0100

    work on the pain expectancy protocol

--- docs/mainz.med.painExpectancy.v1/Script.py
+++ docs/mainz.med.painExpectancy.v1/Script.py
@@ -1,169 +1,114 @@
 import random 
 
-
 class ResponseTask:
-   class PromptState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-
-      def Enter(self):
-         self.tc.Instruments.ImageDisplay.Display(self.tc.Images.Cross)
-         return True
-
-      def Leave(self):
-         return True
-
-      def Update(self):
-         return "Cue" if self.tc.CurrentState.RunningTime > self.owner.PromptDisplayTime else "*"
-
-   class CueState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-         pass
-
-      def CreateImage(self, target, lure):
-         with self.tc.Image.GetCanvas(self.tc.Instruments.ImageDisplay, "#000000") as canvas:
-            w = self.tc.Instruments.ImageDisplay.Width
-            h = self.tc.Instruments.ImageDisplay.Height
-            canvas.AlignCenter()
-            canvas.AlignMiddle()
-            canvas.Color("#FFFFFF")
-            canvas.Write(w/2, h/2, "Cues: {}/{}".format(target, lure))
-            return canvas.GetImage()
-
-      def Enter(self):
-         self.tc.Instruments.ImageDisplay.Display(self.CreateImage("Target", "Lure"))
-         return True
-
-      def Leave(self):
-         self.tc.Keyboard.Clear();
-         return True
-
-      def Update(self):
-         state = self.tc.CurrentState
-         self.tc.CurrentState.Status = "Remaining time: {} ms".format(self.owner.ResponseTimeLimit - state.RunningTime)
-
-         if self.tc.Keyboard.Pressed("LEFT"):
-            return "Display"
-         if self.tc.Keyboard.Pressed("RIGHT"):
-            return "Display"         
-         if state.RunningTime > self.owner.ResponseTimeLimit:
-            return "Display"
-
-         return "*"
-
-   class DisplayState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-
-      def CreateImage(self, cue):
-         with self.tc.Image.GetCanvas(self.tc.Instruments.ImageDisplay, "#000000") as canvas:
-            w = self.tc.Instruments.ImageDisplay.Width
-            h = self.tc.Instruments.ImageDisplay.Height
-            canvas.AlignCenter()
-            canvas.AlignMiddle()
-            canvas.Color("#FFFFFF")
-            canvas.Write(w/2, h/2, "Selected Cue: {}".format(cue))
-            return canvas.GetImage()
-         
-      def Enter(self):
-         self.tc.Instruments.ImageDisplay.Display(self.CreateImage("Selected cue"))
-         return True
-
-      def Leave(self):
-         self.tc.Keyboard.Clear();
-         return True
-
-      def Update(self):
-         return "Stimulate" if self.tc.CurrentState.RunningTime > self.owner.CueDisplayTime else "*"
-
-   class StimulateState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-         pass
-
-      def Enter(self):
-         self.tc.Instruments.ImageDisplay.Display(self.tc.Images.Stimulating)
-         return True
-
-      def Leave(self):
-         return True
-
-      def Update(self):
-         return "Rate" if self.tc.CurrentState.RunningTime > self.owner.StimulationTime else "*"
-
-   class RateState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-         pass
-
-      def PlotRating(self, x, y):
-         with self.tc.Image.GetCanvas(x, y, "#FFFFFF") as canvas:
-            canvas.AlignCenter()
-            canvas.AlignMiddle()
-            canvas.Color("#000000")
-            canvas.Write(x /2, y /2, "Rating: {}".format(self.owner.current))
-            return canvas.GetImage()
-
-      def Enter(self):
-         pass
-
-      def Leave(self):
-         pass
-
-      def Update(self):
-         pass
-
-   class PauseState:
-      def __init__(self, tc, owner):
-         self.owner = owner
-         self.tc = tc
-         pass
-
-      def Enter(self):
-         pass
-
-      def Leave(self):
-         pass
-
-      def Update(self):
-         pass
-
    def __init__(self, tc):
       self.tc = tc
-      self.states = {
-         "Prompt": self.PromptState(tc, self),
-         "Cue": self.CueState(tc, self),
-         "Display": self.DisplayState(tc, self),
-         "Stimulate": self.StimulateState(tc, self),
-         "Rate": self.RateState(tc, self),
-         "Pause": self.PauseState(tc, self),
-      }
       self.PromptDisplayTime = 250  # milliseconds
       self.ResponseTimeLimit = 8000  # milliseconds
       self.CueDisplayTime = 2000  # milliseconds
       self.StimulationTime = 2000  # milliseconds
       self.Cues = [(4,5),(3,6),(4,6),(3,7),(5,6),(4,7)]
 
+   def CreateCueImage(self, target, lure):
+      with self.tc.Image.GetCanvas(self.tc.Instruments.ImageDisplay, "#000000") as canvas:
+         w = self.tc.Instruments.ImageDisplay.Width
+         h = self.tc.Instruments.ImageDisplay.Height
+         canvas.AlignCenter()
+         canvas.AlignMiddle()
+         canvas.Color("#FFFFFF")
+         canvas.Write(w/2, h/2, "Cues: {}/{}".format(target, lure))
+         return canvas.GetImage()
+
+   def CreateSelectedImage(self, cue):
+      with self.tc.Image.GetCanvas(self.tc.Instruments.ImageDisplay, "#000000") as canvas:
+         w = self.tc.Instruments.ImageDisplay.Width
+         h = self.tc.Instruments.ImageDisplay.Height
+         canvas.AlignCenter()
+         canvas.AlignMiddle()
+         canvas.Color("#FFFFFF")
+         canvas.Write(w/2, h/2, "Selected Cue: {}".format(cue))
+         return canvas.GetImage()
+
+   def PlotRating(self, x, y):
+      with self.tc.Image.GetCanvas(x, y, "#FFFFFF") as canvas:
+         canvas.AlignCenter()
+         canvas.AlignMiddle()
+         canvas.Color("#000000")
+         canvas.Write(x /2, y /2, "Rating: {}".format(self.current))
+         return canvas.GetImage()
+
    def Start(self):
+      self.current = 0
       return True
    
    def Complete(self):
       return True
      
    def Enter(self):
-      return self.states[self.tc.CurrentState.ID].Enter()
+      id = self.tc.CurrentState.ID
+
+      if id == "Prompt":
+         self.tc.Instruments.ImageDisplay.Display(self.tc.Images.Cross)
+         return True
+      if id == "Cue":
+         self.tc.Instruments.ImageDisplay.Display(self.CreateCueImage("Target", "Lure"))
+         self.tc.Instruments.Button.Reset()
+         return True
+      if id == "Display":
+         self.tc.Instruments.ImageDisplay.Display(self.CreateSelectedImage("Selected cue"))
+         return True
+      if id == "Stimulate":
+         self.tc.Instruments.ImageDisplay.Display(self.tc.Images.Stimulating)
+         return True
+      if id == "Rate":
+         self.tc.Instruments.Button.Reset()
+         return True
+      if id == "Pause":
+         return True
+
+      return False
     
    def Leave(self):
-      return self.states[self.tc.CurrentState.ID].Leave()
+      return True
    
    def Update(self):
-      return self.states[self.tc.CurrentState.ID].Update()
+      id = self.tc.CurrentState.ID
+
+      if id == "Prompt":
+         return "Cue" if self.tc.CurrentState.RunningTime > self.PromptDisplayTime else "*"
+      
+      if id == "Cue":
+         state = self.tc.CurrentState
+         self.tc.CurrentState.Status = "Remaining time: {} ms".format(self.ResponseTimeLimit - state.RunningTime)
+
+         if self.tc.Instruments.Button.IsLatched("left"):
+            return "Display"
+         if self.tc.Instruments.Button.IsLatched("right"):
+            return "Display"         
+         if state.RunningTime > self.ResponseTimeLimit:
+            return "Display"
+
+         return "*"
+      
+      if id == "Display":
+         return "Stimulate" if self.tc.CurrentState.RunningTime > self.CueDisplayTime else "*"
+      
+      if id == "Stimulate":
+         return "Rate" if self.tc.CurrentState.RunningTime > self.StimulationTime else "*"
+      
+      if id == "Rate":
+         self.current = self.tc.Instruments.Scale.GetCurrentRating()
+
+         if self.tc.Instruments.Button.IsLatched("next"):
+            return "Pause"
+         
+         return "*"   
+         
+      if id == "Pause":
+         return "Prompt" if self.tc.CurrentState.RunningTime > 1000 else "*"
+      
+      return "abort"
+
 
 def CreateTask(tc):
    return ResponseTask(tc)
