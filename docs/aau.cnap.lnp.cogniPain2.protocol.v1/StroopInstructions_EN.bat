2047e4925aaba7788dedf42079630415acf52cd9
Kristian Hennings <krhe@inventors.dk>
2026-06-26 11:46:47 +0200

    Created bat files

--- docs/aau.cnap.lnp.cogniPain2.protocol.v1/StroopInstructions_EN.bat
+++ docs/aau.cnap.lnp.cogniPain2.protocol.v1/StroopInstructions_EN.bat
@@ -5,11 +5,4 @@ move Slide3.PNG NeutralStroopPAD.png
 move Slide4.PNG StroopPAD.png
 cd ..
 
-cd StroopInstructions_DA
-move Slide1.PNG NeutralStroopJoystick.png
-move Slide2.PNG StroopJoystick.png
-move Slide3.PNG NeutralStroopPAD.png
-move Slide4.PNG StroopPAD.png
-cd ..
-
 pause
