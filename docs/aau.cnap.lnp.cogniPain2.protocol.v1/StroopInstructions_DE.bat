d17728860e6e1a9aec10e9d58dd4a2d0c441ee79
Kristian Hennings <krhe@inventors.dk>
2026-06-26 10:43:56 +0200

    Added german instructions for stroop and stop signal

---
+++ docs/aau.cnap.lnp.cogniPain2.protocol.v1/StroopInstructions_DE.bat
@@ -0,0 +1,9 @@
+cd StroopInstructions_DE
+move Slide1.PNG NeutralStroopJoystick.png
+move Slide2.PNG StroopJoystick.png
+move Slide3.PNG NeutralStroopPAD.png
+move Slide4.PNG StroopPAD.png
+cd ..
+
+
+pause
