7ff904acc8d9e5094f395e686aad196c8593b7a2
Kristian Hennings <krhe@inventors.dk>
2026-06-26 11:23:38 +0200

    Updated bat files for localization

--- docs/aau.cnap.lnp.cogniPain2.protocol.v1/CPAInstructions_EN.bat
+++ docs/aau.cnap.lnp.cogniPain2.protocol.v1/CPAInstructions_EN.bat
@@ -5,11 +5,4 @@ move Slide3.PNG TS.png
 move Slide4.PNG CPM.png
 cd ..
 
-cd CPAInstructions_DA
-move Slide1.PNG SR01.png
-move Slide2.PNG SR02.png
-move Slide3.PNG TS.png
-move Slide4.PNG CPM.png
-cd ..
-
 pause
